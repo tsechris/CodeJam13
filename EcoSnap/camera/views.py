d4075af73f895d6905d952e061dabd4635263de3
Blacklotus89898 <song.yang.chen89898@gmail.com>
2023-11-17 22:10:03 -0500

    webcam, post request in base64

--- EcoSnap/camera/views.py
+++ EcoSnap/camera/views.py
@@ -3,6 +3,16 @@ from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
 def camera(r):
+    if r.method == "POST":
+        img_link = str(r.POST['img_link'])
+        img_link = img_link[22:]
+        print(img_link)
+
+
+
     tmp = loader.get_template('camera.html')
     return HttpResponse(tmp.render())
