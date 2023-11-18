ffa6515cb8217670978671647ab463ff241a2c8b
Jiaqi Yang <jiayang@outlook.fr>
2023-11-18 14:30:48 -0500

    added emissions saved and history database

--- EcoSnap/game/models.py
+++ EcoSnap/game/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class RecycleStats(models.Model):
@@ -7,7 +8,13 @@ class RecycleStats(models.Model):
     plastic = models.PositiveIntegerField()
     glass = models.PositiveIntegerField()
     metal = models.PositiveIntegerField()
+    paper = models.PositiveIntegerField(default = 0)
     trash = models.PositiveIntegerField()
     compost = models.PositiveIntegerField()
     daily_streak = models.PositiveIntegerField()
     has_recycled_today = models.BooleanField(default=False)
+
+class History(models.Model):
+    user = models.CharField(max_length=100)
+    day = models.DateField(default=date.today)
+    item = models.CharField(max_length=50)
