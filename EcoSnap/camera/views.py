72ce08ff1a4e42cab3926d7dbe6fb64a944d5b05
Blacklotus89898 <song.yang.chen89898@gmail.com>
2023-11-17 21:15:17 -0500

    adding the app + link

--- EcoSnap/camera/views.py
+++ EcoSnap/camera/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+def camera(r):
+    tmp = loader.get_template('camera.html')
+    return HttpResponse(tmp.render())
