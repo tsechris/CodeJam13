8c66820d3b29ea0aa5b3ee64c828da875366a447
Coldstuff1 <samuel.faubert@mail.mcgill.ca>
2023-11-19 02:21:58 -0500

    updated bash script

--- conda_init.sh
+++ conda_init.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if conda info --envs | grep -q codejam; then echo "base already exists"; else conda create --name codejam python==3.11 pip; fi
+if conda info --envs | grep -q codejam; then echo "codejam already exists"; else conda create --name codejam python==3.11 pip; fi
 
 eval "$(conda shell.bash hook)"
 
 conda activate codejam
 
-pip install numpy
+conda install cudatoolkit
 
-pip install torch
+pip3 install torch torchvision torchaudio
+
+pip install numpy
 
 pip install Django==4.2
 
@@ -19,3 +21,7 @@ pip install requests
 pip install plotpy
 
 pip install plotvision
+
+pip install pathlib
+
+pip install opendatasets
