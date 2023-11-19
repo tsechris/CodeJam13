7812680468120282a9a0ea76e8fbec56485dafbe
Coldstuff1 <samuel.faubert@mail.mcgill.ca>
2023-11-18 23:11:00 -0500

    wrote function to predict image

--- EcoSnap/camera/views.py
+++ EcoSnap/camera/views.py
@@ -5,6 +5,52 @@ from django.http import HttpResponse
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 
+import torch
+from PIL import Image
+import torchvision.transforms.v2 as v2
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.dropout = nn.Dropout(0.7)
+        self.conv1 = nn.Conv2d(3, 20, kernel_size=(5, 5))
+        self.conv2 = nn.Conv2d(20, 50, kernel_size=(5, 5))
+        self.fc1 = nn.Linear(50*53*53, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.logSoftMax = nn.LogSoftmax(dim=1)
+    
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.dropout(x)
+        x = self.pool(self.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return self.logSoftMax(x)
+
+def predictImage(imgPath, modelPath):
+    transformer = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.uint8, scale=True),
+    v2.Resize(size=(224, 224), antialias=True),
+    v2.ToDtype(torch.float32, scale=True),  # Normalize expects float input
+    v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+    img = Image.open(imgPath).convert('RGB')
+    tensor = transformer(img)
+
+    model = CNN()
+    model.load_state_dict(torch.load(modelPath))
+    model.eval()
+
+    _, prediction = torch.max(model(tensor), 1)
+    return prediction.item()
+
 @csrf_exempt
 def index(r):
     print(r)
